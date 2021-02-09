2ba50e2a6f24a244379bb83d49fa964d7f025dc3
Argentina Garcia <margent.garz1926@gmail.com>
2021-02-08 19:30:56 -0600

    Primer commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab1_KASR_MAGZ.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
